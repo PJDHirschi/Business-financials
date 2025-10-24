531ccff9fdfb328c207bd65b6e83d69fa6d1ab0f
technoprism21 <pjdhirschi@yahoo.com>
2025-10-24 11:35:42 -0400

    got rid of extras

--- huggingface test/cudaTest.py
+++
@@ -1,6 +0,0 @@
-import torch
-print("CUDA available:", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("GPU:", torch.cuda.get_device_name(0))
-    print("torch cuda:", torch.version.cuda)
-    print("cudnn:", torch.backends.cudnn.version())
